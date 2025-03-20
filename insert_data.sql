18da70b336180b11740bc756d9d60a55a87dc88b
180519 <161090823+180519@users.noreply.github.com>
2025-03-20 00:04:43 -0600

    Update insert_data.sql

--- insert_data.sql
+++ insert_data.sql
@@ -13,7 +13,9 @@ INSERT INTO asistencia VALUES
 (2,'180259','12-03-2023',false),
 (2,'180519','11-03-2023',false),
 (2,'180774','11-03-2023',true),
-(2,'180259','11-03-2023',false),
+(2,'180259','11-03-2023',false);
+
+INSERT INTO asistencia VALUES
 (1,'180519','12-03-2025',true),
 (1,'180774','12-03-2025',true),
 (1,'180259','12-03-2025',false),
@@ -21,4 +23,5 @@ INSERT INTO asistencia VALUES
 (1,'180774','20-03-2025',false),
 (1,'180259','20-03-2025',false);
 
-
+INSERT INTO asistencia VALUES
+(2,'180519','20-03-2025',true);
