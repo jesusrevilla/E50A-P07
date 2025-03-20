1d8da466e226795e1b4ab057e63935a280512844
AntonioMoralesQuiroz <176412@upslp.edu.mx>
2025-03-19 21:43:05 -0600

    Update purge.sql

--- purge.sql
+++ purge.sql
@@ -1,10 +1,16 @@
 CREATE OR REPLACE FUNCTION purgar_asistencia_antigua()
 RETURNS VOID AS $$
+DECLARE
+    registros_borrados INT;
 BEGIN
     DELETE FROM asistencia
-    WHERE fecha < CURRENT_DATE - INTERVAL '6 months';
+    WHERE fecha < CURRENT_DATE - INTERVAL '6 months'
+    RETURNING id INTO registros_borrados;
+
+    IF FOUND THEN
+        RAISE NOTICE 'Registros eliminados: %', registros_borrados;
+    ELSE
+        RAISE NOTICE 'No se encontraron registros antiguos para eliminar';
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
-
-
-SELECT purgar_asistencia_antigua();
