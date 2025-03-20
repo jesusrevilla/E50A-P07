aed88cf80ab00096b4930c7794c41b808b7ee9fc
AntonioMoralesQuiroz <176412@upslp.edu.mx>
2025-03-19 21:38:49 -0600

    Update drop_data.sql

--- drop_data.sql
+++ drop_data.sql
@@ -1,9 +1,1 @@
--- fetch 
-SELECT * FROM grupos;
--- fetch 
-SELECT * FROM estudiantes;
-
-SELECT * FROM asistencia;
-
-SELECT * FROM asistencia a
-JOIN estudiantes e ON a.id_estudiantes = e.id;
+DROP TABLE asistencia;
