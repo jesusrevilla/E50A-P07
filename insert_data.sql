94324c76e1bce025b65f3c1e826eea2d2bacf00b
180519 <161090823+180519@users.noreply.github.com>
2025-03-19 23:54:16 -0600

    Update insert_data.sql

--- insert_data.sql
+++ insert_data.sql
@@ -1,3 +1,24 @@
+INSERT INTO estudiantes VALUES 
+('180519','Christian Alejandro Cárdenas Rucoba'),
+('180774','Samuel Martínez Luna'),
+('180259','Estefanía Montserrat Monreal Puente');
 
+INSERT INTO grupos(materia,periodo) VALUES
+('Base de Datos','Primavera'),
+('Sistemas Digitales','Otoño');
+
+INSERT INTO asistencia VALUES
+(2,'180519','12-03-2023',true),
+(2,'180774','12-03-2023',true),
+(2,'180259','12-03-2023',false),
+(2,'180519','11-03-2023',false),
+(2,'180774','11-03-2023',true),
+(2,'180259','11-03-2023',false),
+(1,'180519','12-03-2025',true),
+(1,'180774','12-03-2025',true),
+(1,'180259','12-03-2025',false),
+(1,'180519','20-03-2025',false),
+(1,'180774','20-03-2025',false),
+(1,'180259','20-03-2025',false);
 
 
