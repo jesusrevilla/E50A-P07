63080c57813ee817dfd2f35382534f5fa708a326
vulletz <122696042+vulletz@users.noreply.github.com>
2025-03-21 14:42:31 -0600

    Update query_data.sql

--- query_data.sql
+++ query_data.sql
@@ -1,10 +1,5 @@
 \echo '\n\tCONSULTA BASICA\n'
 
-SELECT a.estudiante_id AS Matricula, 
-  e.nombre AS Estudiante, 
-  g.nombre AS Materia, 
-  a.fecha AS Fecha_asistencia, 
-  a.presente AS asistencia 
-FROM asistencia a 
-JOIN estudiantes e ON e.id = a.estudiante_id
-JOIN grupos g ON e.grupo_id = g.id;
+SELECT * FROM grupos; 
+SELECT * FROM estudiantes;
+SELECT * FROM asistencia;
