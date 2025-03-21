53a36b082e807fa4be67903b5941f7caffb90826
170973 <170973@upslp.edu.mx>
2025-03-20 21:40:33 -0600

    Update schema.sql

---
+++ schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE grupos (
+  Id SERIAL PRIMARY KEY,
+  nombre_grupo VARCHAR(30) NOT NULL,
+  clave_grupo   VARCHAR(10)
+);
+
+CREATE TABLE estudiantes (
+  Id SERIAL PRIMARY KEY,
+  nombre_estudiante VARCHAR(50) NOT NULL,
+  carrera VARCHAR(15) NOT NULL,
+  ID_Personal INT NOT NULL,
+  Id_grupo INT NOT NULL,
+  FOREIGN KEY (Id_grupo) references grupos(Id)
+);
+
+CREATE TABLE asistencias (
+  Id SERIAL PRIMARY KEY,
+  fecha VARCHAR(30) NOT NULL,
+  hora VARCHAR(30) NOT NULL,
+  asis boolean,
+  ID_estudiante INT NOT NULL,
+  FOREIGN KEY (ID_estudiante)references estudiantes(Id)
+  
+);
